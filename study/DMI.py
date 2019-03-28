1febe4eed03c8365beb44b1cf5d3e96c17a35445
Tim zhong <2225189600@qq.com>
2019-03-28 12:17:53 +0800

    v0.2

--- study/DMI.py
+++ study/DMI.py
@@ -31,28 +31,77 @@ def no_space(list_l):
     return new_list
 
 
-def report_writer(file_path, position, value ):
+def report_writer(file_path, position, value,style):
     rd_book = xlrd.open_workbook(file_path, formatting_info=True)
     wt_book = copy(rd_book)
-    wt_book.get_sheet(0).write(*position, value)
+    wt_book.get_sheet(0).write(*position, value, style)
+
     wt_book.save(file_path)
 
 
-def report_writer_without_save(file_path, position, value ):
+def report_writer_repeat(file_path, position, value, style):
     rd_book = xlrd.open_workbook(file_path, formatting_info=True)
     wt_book = copy(rd_book)
-    wt_book.get_sheet(0).write(*position, value)
+    wt_book.get_sheet(0).write(*position, value, style)
     return wt_book
 
 
-def wirte_by_type(type_start,type_end,rd_book,new_l,xls_type_line_sn,x):
+
+def report_writer_without_save(file_path, position, value,style ):
+    rd_book = xlrd.open_workbook(file_path, formatting_info=True)
+    wt_book = copy(rd_book)
+    wt_book.get_sheet(0).write(*position, value,style)
+    return wt_book
+
+
+def wirte_by_type(type_start,type_end,rd_book,new_l,xls_type_line_sn,x, style):
     for j in range(xls_type_line_sn[type_start], xls_type_line_sn[type_end]):
         if rd_book.sheet_by_index(0).cell(j, 1).value == new_l[i][0]:
             x = x + 1
 
             print(str(x) + '=> ' + new_l[i][0])
             position = (j, 4)
-            report_writer(file_path, position, new_l[i][2])
+            report_writer(file_path, position, new_l[i][2],style)
+
+
+def wirte_by_repeat_type(type_start,type_end, rd_book, new_l, xls_type_line_sn, x, style):
+    rd_book = xlrd.open_workbook(file_path, formatting_info=True)
+    for j in range(xls_type_line_sn[type_start], xls_type_line_sn[type_end]):
+        if rd_book.sheet_by_index(0).cell(j, 1).value == new_l[i][0]:
+            x = x + 1
+            print(str(x) + '=> ' + new_l[i][0])
+            position = (j, 4)
+            h2 =new_l[i][2]
+            report_writer(file_path, position, rd_book.sheet_by_index(0).cell(j, 4).value + '\n' + new_l[i][2],style)
+
+
+
+
+style = xlwt.XFStyle()#格式信息
+font = xlwt.Font()#字体基本设置
+font.name = u'Arial'
+font.colour_index = 4
+font.height= 220 #字体大小，220就是11号字体，大概就是11*20得来的吧
+style.font = font
+alignment = xlwt.Alignment() # 设置字体在单元格的位置
+alignment.horz = xlwt.Alignment.HORZ_CENTER #水平方向
+alignment.vert = xlwt.Alignment.VERT_CENTER #竖直方向
+style.alignment = alignment
+border = xlwt.Borders()  #给单元格加框线
+border.left = xlwt.Borders.THIN  #左
+border.top=xlwt.Borders.THIN     #上
+border.right=xlwt.Borders.THIN   #右
+border.bottom=xlwt.Borders.THIN  #下
+border.left_colour = 0x40  #设置框线颜色，0x40是黑色，颜色真的巨多，都晕了
+border.right_colour = 0x40
+border.top_colour = 0x40
+border.bottom_colour = 0x40
+style.borders = border
+
+
+
+
+
 
 filename = 'dmi.txt'
 file_path = 'SMBIOS Table Checklist v1.8.xls'
@@ -73,8 +122,12 @@ xls_type_line_sn = {'Type 000': 1, 'Type 001': 11, 'Type 002': 22, 'Type 003': 3
 for line in new_l:
     if line[0].startswith('[Type'):
         type_list.append(line[0][1:9])
-        type_line_sn[line[0][1:9]] = new_l.index(line)
-
+        if line[0][1:9] in type_line_sn:
+            pass
+        else:
+            type_line_sn[line[0][1:9]] = new_l.index(line)
+        print(line[0][1:9],type_line_sn[line[0][1:9]])
+print(type_line_sn)
 print(new_l)
 o=0
 for l in new_l:
@@ -85,6 +138,8 @@ for l in new_l:
 rd_book = xlrd.open_workbook(file_path, formatting_info=True)
 
 x=0
+print(type_line_sn['Type 010'])
+print(type_line_sn['Type 011'])
 for i in range(len(new_l)):
     if i< type_line_sn['Type 000']:
         pass
@@ -94,15 +149,15 @@ for i in range(len(new_l)):
                 x=x+1
                 print(str(x)+'=> '+new_l[i][0])
                 position = (j, 4)
-                report_writer(file_path, position, new_l[i][2])
+                report_writer(file_path, position, new_l[i][2],style)
     elif type_line_sn['Type 001'] < i < type_line_sn['Type 002']:
-        wirte_by_type('Type 001', 'Type 002', rd_book, new_l, xls_type_line_sn, x)
+        wirte_by_type('Type 001', 'Type 002', rd_book, new_l, xls_type_line_sn, x, style)
     elif type_line_sn['Type 002'] < i < type_line_sn['Type 003']:
-        wirte_by_type('Type 002', 'Type 003', rd_book, new_l, xls_type_line_sn, x)
+        wirte_by_type('Type 002', 'Type 003', rd_book, new_l, xls_type_line_sn, x, style)
     elif type_line_sn['Type 003'] < i < type_line_sn['Type 004']:
-        wirte_by_type('Type 003', 'Type 004', rd_book, new_l, xls_type_line_sn, x)
+        wirte_by_type('Type 003', 'Type 004', rd_book, new_l, xls_type_line_sn, x, style)
     elif type_line_sn['Type 004'] < i < type_line_sn['Type 007']:
-        wirte_by_type('Type 004', 'Type 007', rd_book, new_l, xls_type_line_sn, x)
+        wirte_by_type('Type 004', 'Type 007', rd_book, new_l, xls_type_line_sn, x, style)
     elif type_line_sn['Type 011'] < i < type_line_sn['Type 016']:
         for j in range(xls_type_line_sn['Type 011'], xls_type_line_sn['Type 016']):
             if rd_book.sheet_by_index(0).cell(j, 1).value == new_l[i][0]:
@@ -111,27 +166,53 @@ for i in range(len(new_l)):
                 if rd_book.sheet_by_index(0).cell(j, 1).value == 'String #1':
                     print(str(x) + '=> ' + new_l[i][0] + '+++++++++++++++++++++++')
                     print('x==', new_l[i][2],'y==', new_l[i + 1][2])
-                    report_writer(file_path, position, new_l[i][2] + '\n' + new_l[i + 1][2])
+                    report_writer(file_path, position, new_l[i][2] + '\n' + new_l[i + 1][2],style)
                 else:
                     print(str(x) + '=> ' + new_l[i][0] + '===================')
-                    report_writer(file_path, position, new_l[i][2])
+                    report_writer(file_path, position, new_l[i][2],style)
     elif type_line_sn['Type 016'] < i < type_line_sn['Type 017']:
-        wirte_by_type('Type 016', 'Type 017', rd_book, new_l, xls_type_line_sn, x)
+        wirte_by_type('Type 016', 'Type 017', rd_book, new_l, xls_type_line_sn, x, style)
     elif type_line_sn['Type 019'] < i < type_line_sn['Type 020']:
-        wirte_by_type('Type 019', 'Type 020', rd_book, new_l, xls_type_line_sn, x)
+        wirte_by_type('Type 019', 'Type 020', rd_book, new_l, xls_type_line_sn, x, style)
     elif type_line_sn['Type 024'] < i < type_line_sn['Type 032']:
-        wirte_by_type('Type 024', 'Type 032', rd_book, new_l, xls_type_line_sn, x)
+        wirte_by_type('Type 024', 'Type 032', rd_book, new_l, xls_type_line_sn, x, style)
     elif type_line_sn['Type 032'] < i < type_line_sn['Type 041']:
-        wirte_by_type('Type 032', 'Type 041', rd_book, new_l, xls_type_line_sn, x)
+        wirte_by_type('Type 032', 'Type 041', rd_book, new_l, xls_type_line_sn, x, style)
     elif type_line_sn['Type 043'] < i < len(new_l):
-        wirte_by_type('Type 043', 'end', rd_book, new_l, xls_type_line_sn, x)
+        wirte_by_type('Type 043', 'end', rd_book, new_l, xls_type_line_sn, x, style)
     elif type_line_sn['Type 007'] < i < type_line_sn['Type 009']:
-        pass
-    elif type_line_sn['Type 009'] < i < type_line_sn['Type 009']:
-        pass
-    elif type_line_sn['Type 010'] < i < type_line_sn['Type 009']:
-        pass
-    elif type_line_sn['Type 017'] < i < type_line_sn['Type 009']:
-        pass
-    elif type_line_sn['Type 020'] < i < type_line_sn['Type 009']:
-        pass
+        if len(new_l[i]) == 1:
+            continue
+        else:
+            wirte_by_repeat_type('Type 007', 'Type 009', rd_book, new_l, xls_type_line_sn, x, style)
+            pass
+    elif type_line_sn['Type 009'] < i < type_line_sn['Type 010']:
+        if len(new_l[i]) == 1:
+            continue
+        else:
+            wirte_by_repeat_type('Type 009', 'Type 010', rd_book, new_l, xls_type_line_sn, x, style)
+            pass
+    elif type_line_sn['Type 010'] < i < type_line_sn['Type 011']:
+        if len(new_l[i]) == 1:
+            continue
+        else:
+            wirte_by_repeat_type('Type 010', 'Type 011', rd_book, new_l, xls_type_line_sn, x, style)
+            pass
+    elif type_line_sn['Type 017'] < i < type_line_sn['Type 019']:
+        if len(new_l[i]) == 1:
+            continue
+        else:
+            wirte_by_repeat_type('Type 017', 'Type 019', rd_book, new_l, xls_type_line_sn, x, style)
+            pass
+    elif type_line_sn['Type 020'] < i < type_line_sn['Type 024']:
+        if len(new_l[i]) == 1:
+            continue
+        else:
+            wirte_by_repeat_type('Type 020', 'Type 024', rd_book, new_l, xls_type_line_sn, x, style)
+            pass
+    elif type_line_sn['Type 041'] < i < type_line_sn['Type 043']:
+        if len(new_l[i]) == 1:
+            continue
+        else:
+            wirte_by_repeat_type('Type 041', 'Type 043', rd_book, new_l, xls_type_line_sn, x, style)
+            pass
