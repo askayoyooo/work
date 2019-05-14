bf19d597ad1d62953a2b35b44441d424d9f058ed
Tim zhong <2225189600@qq.com>
2019-05-14 15:32:50 +0800

    V0.1 更新DMI_SOP，添加Log 特定路径logs

--- study/DMI_v0.1.py
+++ study/DMI_v0.1.py
@@ -1,7 +1,7 @@
 import xlrd
 import xlwt
 from xlutils.copy import copy
-import os, shutil
+import os, shutil, time
 
 
 def get_clean_data(filename):
@@ -99,89 +99,94 @@ def main():
     style.borders = border
 
     original_model = 'SMBIOS Table Checklist v1.8.xls'
-    filename = file_name_p(os.path.dirname(os.path.realpath(__file__)), '.txt')[0]
-    shutil.copy(original_model, filename[0:-3]+'.xls')
-    file_path = filename[0:-3]+'.xls'
+    filenames = file_name_p(os.path.dirname(os.path.realpath(__file__))+'\\logs', '.txt')
+    print(filenames)
+    print(os.path.dirname(os.path.realpath(__file__))+'\\logs\\')
+    for filename in filenames:
+        filename = os.path.dirname(os.path.realpath(__file__))+'\\logs\\'+filename
+        shutil.copy(original_model, filename[0:-3]+'xls')
+        file_path = filename[0:-3]+'xls'
 
-    data_list = get_clean_data(filename)
-    new_l = []
-    for i in data_list:
-        new_l.append(no_space(i))
+        data_list = get_clean_data(filename)
+        new_l = []
+        for i in data_list:
+            new_l.append(no_space(i))
 
-    # 获取dictionary: type line sn {'Type 000':5, 'Type 001':19,...]
-    type_line_sn = {}
-    xls_type_line_sn = {'Type 000': 1, 'Type 001': 11, 'Type 002': 22, 'Type 003': 32, 'Type 004': 41, 'Type 007': 66,
-                        'Type 009': 77, 'Type 010': 88, 'Type 011': 93, 'Type 016': 110, 'Type 017': 120,
-                        'Type 019': 148,
-                        'Type 020': 157, 'Type 024': 176, 'Type 032': 234, 'Type 041': 272, 'Type 043': 284, 'end': 298}
-    for line in new_l:
-        if line[0].startswith('[Type'):
-            if line[0][1:9] in type_line_sn:
-                pass
-            else:
-                type_line_sn[line[0][1:9]] = new_l.index(line)
+        # 获取dictionary: type line sn {'Type 000':5, 'Type 001':19,...]
+        type_line_sn = {}
+        xls_type_line_sn = {'Type 000': 1, 'Type 001': 11, 'Type 002': 22, 'Type 003': 32, 'Type 004': 41, 'Type 007': 66,
+                            'Type 009': 77, 'Type 010': 88, 'Type 011': 93, 'Type 016': 110, 'Type 017': 120,
+                            'Type 019': 148,
+                            'Type 020': 157, 'Type 024': 176, 'Type 032': 234, 'Type 041': 272, 'Type 043': 284, 'end': 298}
+        for line in new_l:
+            if line[0].startswith('[Type'):
+                if line[0][1:9] in type_line_sn:
+                    pass
+                else:
+                    type_line_sn[line[0][1:9]] = new_l.index(line)
 
-    rd_book = xlrd.open_workbook(file_path, formatting_info=True)
+        rd_book = xlrd.open_workbook(file_path, formatting_info=True)
 
-    for i in range(len(new_l)):
-        if type_line_sn['Type 000'] < i < type_line_sn['Type 001']:
-            write_by_type('Type 000', 'Type 001', rd_book, new_l, xls_type_line_sn, style, file_path, i)
-        elif type_line_sn['Type 001'] < i < type_line_sn['Type 002']:
-            write_by_type('Type 001', 'Type 002', rd_book, new_l, xls_type_line_sn, style, file_path, i)
-        elif type_line_sn['Type 002'] < i < type_line_sn['Type 003']:
-            write_by_type('Type 002', 'Type 003', rd_book, new_l, xls_type_line_sn, style, file_path, i)
-        elif type_line_sn['Type 003'] < i < type_line_sn['Type 004']:
-            write_by_type('Type 003', 'Type 004', rd_book, new_l, xls_type_line_sn, style, file_path, i)
-        elif type_line_sn['Type 004'] < i < type_line_sn['Type 007']:
-            write_by_type('Type 004', 'Type 007', rd_book, new_l, xls_type_line_sn, style, file_path, i)
-        elif type_line_sn['Type 011'] < i < type_line_sn['Type 016']:
-            for j in range(xls_type_line_sn['Type 011'], xls_type_line_sn['Type 016']):
-                if rd_book.sheet_by_index(0).cell(j, 1).value == new_l[i][0]:
-                    position = (j, 4)
-                    if rd_book.sheet_by_index(0).cell(j, 1).value == 'String #1':
-                        report_writer(file_path, position, new_l[i][2] + '\n' + new_l[i + 1][2], style)
-                    else:
-                        report_writer(file_path, position, new_l[i][2], style)
-        elif type_line_sn['Type 016'] < i < type_line_sn['Type 017']:
-            write_by_type('Type 016', 'Type 017', rd_book, new_l, xls_type_line_sn, style, file_path, i)
-        elif type_line_sn['Type 019'] < i < type_line_sn['Type 020']:
-            write_by_type('Type 019', 'Type 020', rd_book, new_l, xls_type_line_sn, style, file_path, i)
-        elif type_line_sn['Type 024'] < i < type_line_sn['Type 032']:
-            write_by_type('Type 024', 'Type 032', rd_book, new_l, xls_type_line_sn, style, file_path, i)
-        elif type_line_sn['Type 032'] < i < type_line_sn['Type 041']:
-            write_by_type('Type 032', 'Type 041', rd_book, new_l, xls_type_line_sn, style, file_path, i)
-        elif type_line_sn['Type 043'] < i < len(new_l):
-            write_by_type('Type 043', 'end', rd_book, new_l, xls_type_line_sn, style, file_path, i)
-        elif type_line_sn['Type 007'] < i < type_line_sn['Type 009']:
-            if len(new_l[i]) == 1:
-                continue
-            else:
-                write_by_repeat_type('Type 007', 'Type 009', new_l, xls_type_line_sn, style, file_path, i)
-        elif type_line_sn['Type 009'] < i < type_line_sn['Type 010']:
-            if len(new_l[i]) == 1:
-                continue
-            else:
-                write_by_repeat_type('Type 009', 'Type 010', new_l, xls_type_line_sn, style, file_path, i)
-        elif type_line_sn['Type 010'] < i < type_line_sn['Type 011']:
-            if len(new_l[i]) == 1:
-                continue
-            else:
-                write_by_repeat_type('Type 010', 'Type 011', new_l, xls_type_line_sn, style, file_path, i)
-        elif type_line_sn['Type 017'] < i < type_line_sn['Type 019']:
-            if len(new_l[i]) == 1:
-                continue
-            else:
-                write_by_repeat_type('Type 017', 'Type 019', new_l, xls_type_line_sn, style, file_path, i)
-        elif type_line_sn['Type 020'] < i < type_line_sn['Type 024']:
-            if len(new_l[i]) == 1:
-                continue
-            else:
-                write_by_repeat_type('Type 020', 'Type 024', new_l, xls_type_line_sn, style, file_path, i)
-        elif type_line_sn['Type 041'] < i < type_line_sn['Type 043']:
-            if len(new_l[i]) == 1:
-                continue
-            else:
-                write_by_repeat_type('Type 041', 'Type 043', new_l, xls_type_line_sn, style, file_path, i)
+        for i in range(len(new_l)):
+            if type_line_sn['Type 000'] < i < type_line_sn['Type 001']:
+                write_by_type('Type 000', 'Type 001', rd_book, new_l, xls_type_line_sn, style, file_path, i)
+            elif type_line_sn['Type 001'] < i < type_line_sn['Type 002']:
+                write_by_type('Type 001', 'Type 002', rd_book, new_l, xls_type_line_sn, style, file_path, i)
+            elif type_line_sn['Type 002'] < i < type_line_sn['Type 003']:
+                write_by_type('Type 002', 'Type 003', rd_book, new_l, xls_type_line_sn, style, file_path, i)
+            elif type_line_sn['Type 003'] < i < type_line_sn['Type 004']:
+                write_by_type('Type 003', 'Type 004', rd_book, new_l, xls_type_line_sn, style, file_path, i)
+            elif type_line_sn['Type 004'] < i < type_line_sn['Type 007']:
+                write_by_type('Type 004', 'Type 007', rd_book, new_l, xls_type_line_sn, style, file_path, i)
+            elif type_line_sn['Type 011'] < i < type_line_sn['Type 016']:
+                for j in range(xls_type_line_sn['Type 011'], xls_type_line_sn['Type 016']):
+                    if rd_book.sheet_by_index(0).cell(j, 1).value == new_l[i][0]:
+                        position = (j, 4)
+                        if rd_book.sheet_by_index(0).cell(j, 1).value == 'String #1':
+                            report_writer(file_path, position, new_l[i][2] + '\n' + new_l[i + 1][2], style)
+                        else:
+                            report_writer(file_path, position, new_l[i][2], style)
+            elif type_line_sn['Type 016'] < i < type_line_sn['Type 017']:
+                write_by_type('Type 016', 'Type 017', rd_book, new_l, xls_type_line_sn, style, file_path, i)
+            elif type_line_sn['Type 019'] < i < type_line_sn['Type 020']:
+                write_by_type('Type 019', 'Type 020', rd_book, new_l, xls_type_line_sn, style, file_path, i)
+            elif type_line_sn['Type 024'] < i < type_line_sn['Type 032']:
+                write_by_type('Type 024', 'Type 032', rd_book, new_l, xls_type_line_sn, style, file_path, i)
+            elif type_line_sn['Type 032'] < i < type_line_sn['Type 041']:
+                write_by_type('Type 032', 'Type 041', rd_book, new_l, xls_type_line_sn, style, file_path, i)
+            elif type_line_sn['Type 043'] < i < len(new_l):
+                write_by_type('Type 043', 'end', rd_book, new_l, xls_type_line_sn, style, file_path, i)
+            elif type_line_sn['Type 007'] < i < type_line_sn['Type 009']:
+                if len(new_l[i]) == 1:
+                    continue
+                else:
+                    write_by_repeat_type('Type 007', 'Type 009', new_l, xls_type_line_sn, style, file_path, i)
+            elif type_line_sn['Type 009'] < i < type_line_sn['Type 010']:
+                if len(new_l[i]) == 1:
+                    continue
+                else:
+                    write_by_repeat_type('Type 009', 'Type 010', new_l, xls_type_line_sn, style, file_path, i)
+            elif type_line_sn['Type 010'] < i < type_line_sn['Type 011']:
+                if len(new_l[i]) == 1:
+                    continue
+                else:
+                    write_by_repeat_type('Type 010', 'Type 011', new_l, xls_type_line_sn, style, file_path, i)
+            elif type_line_sn['Type 017'] < i < type_line_sn['Type 019']:
+                if len(new_l[i]) == 1:
+                    continue
+                else:
+                    write_by_repeat_type('Type 017', 'Type 019', new_l, xls_type_line_sn, style, file_path, i)
+            elif type_line_sn['Type 020'] < i < type_line_sn['Type 024']:
+                if len(new_l[i]) == 1:
+                    continue
+                else:
+                    write_by_repeat_type('Type 020', 'Type 024', new_l, xls_type_line_sn, style, file_path, i)
+            elif type_line_sn['Type 041'] < i < type_line_sn['Type 043']:
+                if len(new_l[i]) == 1:
+                    continue
+                else:
+                    write_by_repeat_type('Type 041', 'Type 043', new_l, xls_type_line_sn, style, file_path, i)
+    time.sleep(5)
     print('==========================================\n')
     print('Finished 1 SKU result')
     print('==========================================\n')
@@ -189,4 +194,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
